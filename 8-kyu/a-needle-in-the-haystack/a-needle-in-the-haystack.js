a14a0782b568f4bfab3d9e2712f9c4769e2e6d12
Alexander Reznik <alex_reznik@verizon.net>
2025-04-27 17:19:32 -0400

    added comments a-needle-in-the-haystack.js

--- 8-kyu/a-needle-in-the-haystack/a-needle-in-the-haystack.js
+++ 8-kyu/a-needle-in-the-haystack/a-needle-in-the-haystack.js
@@ -1,7 +1,7 @@
-function findNeedle(haystack) {
-    for (let i = 0; i < haystack.length; i++)
-    {if (haystack[i] === 'needle') {
-      return `found the needle at position ${i}`;
+function findNeedle(haystack) {            //defines the function to take one input, a list named 'haystack'
+    for (let i = 0; i < haystack.length; i++) //this loops through each item in a list named 'haystack'
+    {if (haystack[i] === 'needle') {                             //it looks of any item is equal to 'needle'
+      return `found the needle at position ${i}`;               //it returns in which index/positon it finds the 'needle' 
   }
            }
 }
